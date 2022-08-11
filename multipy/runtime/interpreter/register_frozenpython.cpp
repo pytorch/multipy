4bea67941663dd081405034fe9f19d9c1604f430
Anirban Roychowdhury <anirbanr@fb.com>
2022-08-10 23:32:22 -0700

    Rebase.
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- multipy/runtime/interpreter/register_frozenpython.cpp
+++ multipy/runtime/interpreter/register_frozenpython.cpp
@@ -92,9 +92,3 @@ REGISTER_TORCH_DEPLOY_BUILTIN(
     _PyImport_FrozenModules FOREACH_LIBRARY(STD_LIBARY_PARMS));
 #endif
 #undef STD_LIBARY_PARMS
-
-#ifdef INCLUDE_LEGACY_PARSER_MODULE
-extern "C" PyObject* PyInit_parser(void);
-REGISTER_TORCH_DEPLOY_BUILTIN(
-    frozenpython, _PyImport_FrozenModules, "parser", PyInit_parser);
-#endif
