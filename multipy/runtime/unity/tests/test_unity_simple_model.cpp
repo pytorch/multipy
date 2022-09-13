09f5f7a7dbef95c17412aa7c3e343c0d383bddf2
Sahan Paliskara <sahanp@fb.com>
2022-09-12 20:54:33 -0700

    Back out "Remove dead includes in multipy" (#170)
    Summary:
    Pull Request resolved: https://github.com/pytorch/multipy/pull/170
    
    Backout because dead include removals break OSS.
    
    Reviewed By: d4l3k, anirbanr-fb-r2p
    
    Differential Revision: D39449388
    
    fbshipit-source-id: 585b31dc0b79083bd792a8eb62259967ac7e46ab

--- multipy/runtime/unity/tests/test_unity_simple_model.cpp
+++ multipy/runtime/unity/tests/test_unity_simple_model.cpp
@@ -4,6 +4,7 @@
 // This source code is licensed under the BSD-style license found in the
 // LICENSE file in the root directory of this source tree.
 
+#include <fmt/format.h>
 #include <gtest/gtest.h>
 #include <multipy/runtime/unity/tests/test_unity.h>
 #include <multipy/runtime/unity/xar_environment.h>
