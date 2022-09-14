49721042bd1e1160c954590927f31306ec1f5983
Anirban Roychowdhury <anirbanr@fb.com>
2022-09-14 13:01:05 -0700

    Tst pt3d only. Summary:
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- multipy/runtime/test_compat.py
+++ multipy/runtime/test_compat.py
@@ -13,11 +13,11 @@ class TestCompat(unittest.TestCase):
     # def test_torchvision(self):
     #     import torchvision  # noqa: F401
 
-    def test_torchaudio(self):
-        import torchaudio  # noqa: F401
+    # def test_torchaudio(self):
+    #     import torchaudio  # noqa: F401
 
-    # def test_pytorch3d(self):
-    #     import pytorch3d  # noqa: F401
+    def test_pytorch3d(self):
+        import pytorch3d  # noqa: F401
 
     # def test_hf_tokenizers(self):
     #     import tokenizers  # noqa: F401
