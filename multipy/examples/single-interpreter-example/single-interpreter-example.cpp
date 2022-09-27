ab8fa613dc75945054f34c618a752f921aee2c3c
Sahan Paliskara <sahancpal@gmail.com>
2022-09-27 16:31:51 -0700

    single interpreter example
    [ghstack-poisoned]

---
+++ multipy/examples/single-interpreter-example/single-interpreter-example.cpp
@@ -0,0 +1,43 @@
+#include <multipy/runtime/deploy.h>
+#include <multipy/runtime/path_environment.h>
+#include <torch/script.h>
+#include <torch/torch.h>
+
+#include <iostream>
+#include <memory>
+
+int main(int argc, const char* argv[]) {
+    if (argc != 1) {
+        std::cerr << "usage: example-app <path-to-exported-script-module>\n";
+        return -1;
+    }
+
+    // Start an interpreter manager governing 4 embedded interpreters.
+    std::shared_ptr<torch::deploy::Environment> env =
+        std::make_shared<torch::deploy::PathEnvironment>(
+            std::getenv("PATH_TO_EXTERN_PYTHON_PACKAGES") // Ensure to set this environment variable (e.g. /home/user/anaconda3/envs/multipy-example/lib/python3.8/site-packages)
+        );
+    torch::deploy::InterpreterManager m(4, env);
+
+    try {
+        // Load the model from the multipy.package.
+        auto I = m.acquireOne();
+        std::vector<torch::jit::IValue> constructor_inputs;
+        auto model_obj = I.global("torch.nn", "Conv2d")({6, 2, 2, 1});
+        std::vector<torch::jit::IValue> inputs;
+        inputs.push_back(torch::ones({1, 6, 6, 6}));
+
+        // Execute the model and turn its output into a tensor.
+        at::Tensor output = model_obj(inputs).toIValue().toTensor();
+        std::cout << output.slice(/*dim=*/1, /*start=*/0, /*end=*/5) << '\n';
+
+
+
+    } catch (const c10::Error& e) {
+        std::cerr << "error creating movable\n";
+        std::cerr << e.msg();
+        return -1;
+    }
+
+    std::cout << "ok\n";
+}
