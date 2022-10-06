26061f8628ea8a50f2a92720406c3a987ff71cf1
Anirban Roychowdhury <anirbanr@fb.com>
2022-10-06 11:43:50 -0700

    Tst. Summary:
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- setup.py
+++ setup.py
@@ -69,16 +69,16 @@ class MultipyRuntimeBuild(build_ext):
             raise RuntimeError(e.output)
 
         print(f"-- Running multipy runtime install in dir {build_dir_abs}")
-        try:
-            subprocess.run(
-                ["cmake --install . --prefix \".\""],
-                cwd=build_dir_abs,
-                shell=True,
-                check=True,
-                capture_output=True,
-            )
-        except subprocess.CalledProcessError as e:
-            raise RuntimeError(e.output)
+        # try:
+        #     subprocess.run(
+        #         ["cmake --install . --prefix \".\""],
+        #         cwd=build_dir_abs,
+        #         shell=True,
+        #         check=True,
+        #         capture_output=True,
+        #     )
+        # except subprocess.CalledProcessError as e:
+        #     raise RuntimeError(e.output)
         # TODO
         # followups: gen examples, copy .so out.
 
