bf52c0164da448f04bdcf90f12f50784a28a73ec
PaliC <sahancpal@gmail.com>
2022-10-25 21:00:07 +0000

    path env example
    [ghstack-poisoned]

--- examples/path_env/path_env.cpp
+++ examples/path_env/path_env.cpp
@@ -9,22 +9,17 @@
 
 int main(int argc, const char* argv[]) {
   torch::deploy::InterpreterManager m(4);
+    // Start an interpreter manager governing 4 embedded interpreters.
+    std::shared_ptr<multipy::runtime::Environment> env =
+        std::make_shared<multipy::runtime::PathEnvironment>(
+            std::getenv("PATH_TO_EXTERN_PYTHON_PACKAGES") // Ensure to set this environment variable (e.g. /home/user/anaconda3/envs/multipy-example/lib/python3.8/site-packages)
+        );
 
   try {
     // Load the model from the torch.package.
     auto I = m.acquireOne();
     auto model_obj = I.global("torch.nn", "Conv2d")({6, 2, 2, 1});
-    auto rObj = m.createMovable(model_obj, &I);
-    auto I2 = m.acquireOne();
-    auto model_obj2 = I2.fromMovable(rObj);
-    rObj.unload(); // free the replicated object
-
-    // Create a vector of inputs.
-    std::vector<torch::jit::IValue> inputs;
-    inputs.push_back(torch::ones({1, 6, 6, 6}));
-
-    // Execute the model and turn its output into a tensor.
-    at::Tensor output = model_obj2(inputs).toIValue().toTensor();
+    at::Tensor output = model_obj(inputs).toIValue().toTensor();
     std::cout << output.slice(/*dim=*/1, /*start=*/0, /*end=*/5) << '\n';
 
   } catch (const c10::Error& e) {
