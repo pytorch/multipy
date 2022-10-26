2de130bef1cae43f1fb9b8a7a3754dbe12be037f
Sahan Paliskara <sahancpal@gmail.com>
2022-10-26 14:01:42 -0700

    Update on "add docs CI"
    [ghstack-poisoned]

--- docs/doc_push.sh
+++ docs/doc_push.sh
@@ -56,9 +56,9 @@ fi
 
 echo "Installing multipy from $repo_root..."
 cd "$repo_root" || exit
-pip install --upgrade pip
-pip install -r requirements.txt
-pip install -e . --install-option="--cmakeoff"
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+python3 -m pip install -e . --install-option="--cmakeoff"
 
 multipy_ver=$(python -c "import multipy; print(multipy.__version__)")
 
@@ -66,7 +66,7 @@ echo "Building multipy-$multipy_ver docs..."
 docs_dir=$repo_root/docs
 build_dir=$docs_dir/build
 cd "$docs_dir" || exit
-pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 make clean html
 echo "Doc build complete"
 
