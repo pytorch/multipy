d60f34ad38c371e441fe7ffdb77a3c3dda5a5d19
Shiyan Deng <dsy842974287@meta.com>
2023-05-08 23:38:42 -0700

    Back out "Convert multipy embedded library exceptions to specific exception type" (#320)
    Summary:
    Pull Request resolved: https://github.com/pytorch/multipy/pull/320
    
    X-link: https://github.com/pytorch/torchrec/pull/1153
    
    Original commit changeset: 9cb3d1f17f54
    
    Original Phabricator Diff: D44477860
    
    In deploy, we do dlopen to link the .so each time when we create interpreter. So when interpreter manager is out of scope, we dlclose so we no longer have any symbols in the interpreter .so lib https://fburl.com/code/wlcp3r9o.
    
    If we throw a custom exception class that defined in interpreter .so, then in the catch block, if interpreter manager goes out of scope, we don't recognize this exception class anymore and lead to a crash.
    
    Reviewed By: zyan0
    
    Differential Revision: D45629261
    
    fbshipit-source-id: 5606f527e0952ee8a4184de25bd18b42faf772f3

--- multipy/runtime/Exception.h
+++ multipy/runtime/Exception.h
@@ -4,13 +4,10 @@
 // This source code is licensed under the BSD-style license found in the
 // LICENSE file in the root directory of this source tree.
 
-#pragma once
-
 #ifndef MULTIPY_EXCEPTION_H
 #define MULTIPY_EXCEPTION_H
 
 #include <exception>
-#include <stdexcept>
 
 #define MULTIPY_INTERNAL_ASSERT_WITH_MESSAGE(condition, message)               \
   if (!(condition)) {                                                          \
@@ -54,13 +51,3 @@
       MULTIPY_CHECK_NO_MESSAGE(__VA_ARGS__));
 
 #endif // MULTIPY_EXCEPTION_H
-
-namespace torch {
-namespace deploy {
-class MultipyEmbeddedException : public std::runtime_error {
- public:
-  explicit MultipyEmbeddedException(const std::string& error)
-      : std::runtime_error(error) {}
-};
-} // namespace deploy
-} // namespace torch
