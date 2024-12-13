be69fcd92e2994350406cbc693f8bc6e880e22c8
generatedunixname89002005307016 <generatedunixname89002005307016@meta.com>
2024-12-12 20:27:35 -0800

    Add missing Pyre mode headers] [batch:35/1474] [shard:25/N] [B]
    Differential Revision: D67115923
    
    fbshipit-source-id: 291e5338dd9b1551d3f78ec41d92e38696f26b12

--- multipy/runtime/testdev/test_deploy_from_python.py
+++ multipy/runtime/testdev/test_deploy_from_python.py
@@ -4,6 +4,8 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+# pyre-unsafe
+
 import unittest
 
 # pyre-fixme[21]: Could not find module `test_deploy_python_ext`.
--- multipy/runtime/unity/tests/simple_model.py
+++ multipy/runtime/unity/tests/simple_model.py
@@ -4,6 +4,8 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+# pyre-unsafe
+
 import torch
 
 # pyre-fixme[21]: Could not find name `nn` in `torch`.
